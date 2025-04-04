1755bb46b7752d05065a40a9d4eb27ade4bde45a
rahul-rakhe <rahul.rakhe@gmail.com>
2025-04-04 14:13:27 +0530

    Static Variables

---
+++ src/staticMembers/StaticVariables.java
@@ -0,0 +1,13 @@
+package staticMembers;
+
+public class StaticVariables {
+
+	static int num;
+
+	public static void main(String[] args) {
+
+		// It is always a good practise to use the class name to access the static variable
+
+		System.out.println(StaticVariables.num);
+	}
+}
